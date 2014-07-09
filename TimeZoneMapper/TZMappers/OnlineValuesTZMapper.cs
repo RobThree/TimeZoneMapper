5425293fb77cc6cf4a867c56eb5c63fb06e83706
RobThree <rob@devcorner.nl>
2014-07-09 14:26:18 +0200

    * OnlineValuesTZMapper now uses shorter timeout (5 sec), configurable when desired as well as the resource URI.

--- TimeZoneMapper/TZMappers/OnlineValuesTZMapper.cs
+++ TimeZoneMapper/TZMappers/OnlineValuesTZMapper.cs
@@ -1,5 +1,6 @@
 ﻿namespace TimeZoneMapper.TZMappers
 {
+    using System;
     using System.Net;
 
     /// <summary>
@@ -7,8 +8,64 @@
     /// </summary>
     public sealed class OnlineValuesTZMapper : BaseTZMapper, TimeZoneMapper.TZMappers.ITZMapper
     {
-        private const string resourceuri = "http://unicode.org/repos/cldr/trunk/common/supplemental/windowsZones.xml";
-        internal OnlineValuesTZMapper()
-            : base(new WebClient().DownloadString(resourceuri)) { }
+        /// <summary>
+        /// Default URL used for <see cref="OnlineValuesTZMapper"/>
+        /// </summary>
+        public const string DEFAULTRESOURCEURL = "http://unicode.org/repos/cldr/trunk/common/supplemental/windowsZones.xml";
+
+        /// <summary>
+        /// Initializes a new instance of an <see cref="OnlineValuesTZMapper"/> with default timeout of 5 seconds and 
+        /// <see cref="DEFAULTRESOURCEURL"/> as resourceURL.
+        /// </summary>
+        public OnlineValuesTZMapper()
+            : this(5000) { }
+
+
+        /// <summary>
+        /// Initializes a new instance of an <see cref="OnlineValuesTZMapper"/> with the specified timeout and 
+        /// <see cref="DEFAULTRESOURCEURL"/> as resourceURL.
+        /// </summary>
+        /// <param name="timeout">The length of time, in milliseconds, before the request times out.</param>
+        public OnlineValuesTZMapper(int timeout)
+            : this(timeout, DEFAULTRESOURCEURL) { }
+
+        /// <summary>
+        /// Initializes a new instance of an <see cref="OnlineValuesTZMapper"/> with the specified timeout and 
+        /// resourceURL.
+        /// </summary>
+        /// <param name="timeout">The length of time, in milliseconds, before the request times out.</param>
+        /// <param name="resourceurl">The URL to use when retrieving CLDR data.</param>
+        public OnlineValuesTZMapper(int timeout, string resourceurl)
+            : this(timeout, new Uri(resourceurl, UriKind.Absolute)) { }
+
+        /// <summary>
+        /// Initializes a new instance of an <see cref="OnlineValuesTZMapper"/> with the specified timeout and 
+        /// resourceURI.
+        /// </summary>
+        /// <param name="timeout">The length of time, in milliseconds, before the request times out.</param>
+        /// <param name="resourceuri">The URI to use when retrieving CLDR data.</param>
+        public OnlineValuesTZMapper(int timeout, Uri resourceuri)
+            : base(new TimedWebClient(timeout).DownloadString(resourceuri)) { }
+
+
+        /// <summary>
+        /// Simple "wrapper class" providing timeouts.
+        /// </summary>
+        private class TimedWebClient : WebClient
+        {
+            public int Timeout { get; set; }
+
+            public TimedWebClient(int timeout)
+            {
+                this.Timeout = timeout;
+            }
+
+            protected override WebRequest GetWebRequest(Uri address)
+            {
+                var wr = base.GetWebRequest(address);
+                wr.Timeout = this.Timeout;
+                return wr;
+            }
+        }
     }
 }
