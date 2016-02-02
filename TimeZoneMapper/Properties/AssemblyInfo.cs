550d8e51e26dab76e4d7d18f4d50e2d8207e66cf
RobThree <rob@devcorner.nl>
2016-02-02 16:37:20 +0100

    * Updated built-in timezone resource (7df0005:2015g -> 7df0103:2016a)

--- TimeZoneMapper/Properties/AssemblyInfo.cs
+++ TimeZoneMapper/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Devcorner.nl")]
 [assembly: AssemblyProduct("TimeZoneMapper")]
-[assembly: AssemblyCopyright("Copyright © Devcorner.nl 2013 - 2015")]
+[assembly: AssemblyCopyright("Copyright © Devcorner.nl 2013 - 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.7.0")]
-[assembly: AssemblyFileVersion("1.2.7.0")]
+[assembly: AssemblyVersion("1.2.8.0")]
+[assembly: AssemblyFileVersion("1.2.8.0")]
