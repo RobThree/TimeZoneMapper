2b040105094b3749d4293eba8c038ee952be417c
Antoine Sottiau <antoine@ea4t.com>
2022-01-01 20:10:30 +0100

    DefaultValuesTZMapper: public constructor to allow supporting outdated windows versions

--- TimeZoneMapper/TZMappers/DefaultValuesTZMapper.cs
+++ TimeZoneMapper/TZMappers/DefaultValuesTZMapper.cs
@@ -5,7 +5,18 @@
     /// </summary>
     public sealed class DefaultValuesTZMapper : BaseTZMapper, ITZMapper
     {
-        internal DefaultValuesTZMapper()
-            : base(Properties.Resources.windowsZones) { }
+        /// <summary>
+        /// Provides TimeZoneID mapping based on a built-in (&quot;static&quot;) resource.
+        /// </summary>
+        /// <param name="throwOnDuplicateKey">
+        /// When true, an exception will be thrown when the XML data contains duplicate timezones. When false, 
+        /// duplicates are ignored and only the first entry in the XML data will be used.
+        /// </param>
+        /// <param name="throwOnNonExisting">
+        /// When true, an exception will be thrown when the XML data contains non-existing timezone ID's. When false,
+        /// non-existing timezone ID's are ignored.
+        /// </param>
+        public DefaultValuesTZMapper(bool throwOnDuplicateKey = true, bool throwOnNonExisting = true)
+            : base(Properties.Resources.windowsZones, throwOnDuplicateKey, throwOnNonExisting) { }
     }
 }
